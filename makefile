ff23dca5971f1b1e8b8f20dd5fbeaa79fbaaa310
David Merlat <david.merlat28@gmail.com>
2020-11-25 15:27:20 +0100

    nique les undefined reference

--- makefile
+++ makefile
@@ -1,8 +1,2 @@
-all: tp1.o hadamard.o
-	gcc -o prog.exe tp1.o hadamard.o
-
-tp1.o: tp1.c
-	gcc -o tp1.o tp1.c
-
-hadamard.o: hadamard.c
-	gcc -o hadamard.o hadamard.c
+all: tp1.c
+	gcc -o prog.exe tp1.c
--- tp1.c
+++ tp1.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "hadamard.h"
 
 int calculerTailleMatrice(int nbUtilisateur){
     switch(nbUtilisateur){
@@ -36,16 +35,39 @@ int calculerTailleMatrice(int nbUtilisateur){
     return 0;
 }
 
+int fusionnerMatrices(
+    int taille, int tailleFinale,
+    int matriceFinale[][tailleFinale],
+    int matrice1[][taille],
+    int matrice2[][taille],
+    int matrice3[][taille],
+    int matrice4[][taille]
+){
+    for(int i = 0; i < tailleFinale ; i++){
+        printf("%d",taille);
+    }
+    return 0;
+}
+
+extern int genererHadamard(int tailleMatrice, int mat[][tailleMatrice]){
+    if(tailleMatrice > 1){// 4 8 16
+        int ** mat[tailleMatrice/2][tailleMatrice/2];
+    }
+    return 0;
+}
+
 int main(int argc, char* argv[]){
     int nbUtilisateurs = 0;
     if(argc < 2){
-        printf("Attente d'un paramètre : <Nombre_D_Utilisateurs>");
+        printf("Attente d'un paramètre : <Nombre_D_Utilisateurs>\n");
         system("pause");
         return 0;
     }
     nbUtilisateurs = atoi(argv[1]);
     int tailleMatrice = calculerTailleMatrice(nbUtilisateurs);
+    printf("Taille Matrice : %d\n",tailleMatrice);
+    system("pause");
     int mat[tailleMatrice][tailleMatrice];
     genererHadamard(tailleMatrice,mat);
-
+    system("pause");
 }
