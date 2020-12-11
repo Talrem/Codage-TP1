86389607da5114970fc8f81c14033faefe72c4df
ysendark <60466223+ysendark@users.noreply.github.com>
2020-12-11 16:16:40 +0100

    Add files via upload
    les fichier pour gold

--- gold.c
+++ gold.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include "gold.h"
 
 int xor(int a,int b){
     return ((a && !b) || (!a && b));
---
+++ gold.h
@@ -0,0 +1,5 @@
+
+
+int xor(int a,int b);
+void codeurGold(int * polyGen, int taille, int * registres);
+void decalage(int resultXor, int * registres);
