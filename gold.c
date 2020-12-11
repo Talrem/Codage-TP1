c6717c35f0afd7d77a2503a9a5efe4441339a159
ysendark <60466223+ysendark@users.noreply.github.com>
2020-12-11 16:13:06 +0100

    Add files via upload
    le gold finit (normalement)

--- gold.c
+++ gold.c
@@ -1,12 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/*initRegistres(int * registres, taille){ // initialise les registres à 1 pour que l'encodeur puisse démarer
-
-  for(i = 0; i < taille; i++){
-    registres[i] = 1;
-  }
-}*/
 
 int xor(int a,int b){
     return ((a && !b) || (!a && b));
@@ -18,7 +12,7 @@ void codeurGold(int * polyGen, int taille, int * registres){  // realise un coda
     int resultXor = -1;                                                  //de gold, neccesite les plynome de Générétion, la taille et les registres initialisé
     int i,j;
     for(i=0; i < taille ; i++){
-      for(j = 0; polyGen[j] =! '\0'; j++){
+      for(j = 0; polyGen[j] != '\0'; j++){
         if( i == polyGen[j]){
           if(resultXor == -1){
             resultXor = registres[i];
@@ -28,4 +22,16 @@ void codeurGold(int * polyGen, int taille, int * registres){  // realise un coda
         }
       }
     }
+}
+
+void decalage(int resultXor, int * registres){
+
+  int i,j,k;
+  j = registres[0]
+  registres[0] = resultXor;
+  for(i = 1;registres[i] != '\0';i++){
+    k = registres[i];
+    registres[i] = j;
+    j = k;
+  }
 }
